019474f9200106862b931778d450026ab155a92e
Aakarsh Jain <jainaakarsh09@gmail.com>
2022-07-12 20:35:07 +0530

    Completed qualifier.py with own code

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -20,6 +20,7 @@ class RestaurantManager:
         already defined a staff dictionary.
         """
         self.staff = {}
+        self.freq = {}
 
     async def __call__(self, request: Request):
         """Handle a request received.
@@ -31,4 +32,24 @@ class RestaurantManager:
             Request object containing information about the sent
             request to your application.
         """
-        ...
+        if request.scope['type'] == 'staff.onduty':
+            self.staff[request.scope['id']] = request
+            self.freq[request.scope['id']] = 0
+        
+        if request.scope['type'] == 'staff.offduty':
+            del self.staff[request.scope['id']]
+            del self.freq[request.scope['id']]
+
+        if request.scope['type'] == 'order':
+            spec = request.scope['speciality']
+            poss = []
+            for staff_id in self.staff:
+                if spec in self.staff[staff_id].scope['speciality']:
+                    poss.append(staff_id)
+            id_chosen = min(poss, key = lambda id:self.freq[id])
+            staff_chosen = self.staff[id_chosen]
+            self.freq[id_chosen] += 1
+            full_order = await request.receive()
+            await staff_chosen.send(full_order)
+            res = await staff_chosen.receive()
+            await request.send(res)
